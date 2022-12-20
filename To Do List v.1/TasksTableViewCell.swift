8c45c9c054f07d80ce67c0710665a91111846edf
aamer Essa <aamer.es12@gmail.com>
2022-12-21 00:25:20 +0300

    V1.0

---
+++ To Do List v.1/TasksTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TasksTableViewCell.swift
+//  To Do List v.1
+//
+//  Created by Aamer Essa on 20/12/2022.
+//
+
+import Cocoa
+
+class TasksTableViewCell: NSTableCellView {
+
+    @IBOutlet weak var taskTitel: NSTextField!
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
--- To Do List v.1/ViewController.swift
+++ To Do List v.1/ViewController.swift
@@ -9,9 +9,18 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var taskTable: NSTableView!
+    @IBOutlet weak var finish: NSButton!
+    @IBOutlet weak var taskTextField: NSTextField!
+    var tasksList = ["go to gym","go to Qatar"]
+    var selectedValue = Int()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        taskTable.dataSource = self
+        taskTable.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
@@ -22,5 +31,35 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func addNewTask(_ sender: NSTextField) {
+        tasksList.append(sender.stringValue)
+        taskTable.reloadData()
+        taskTextField.stringValue = ""
+    }
+    @IBAction func finish(_ sender: Any) {
+        tasksList.remove(at: selectedValue)
+        taskTable.reloadData()
+    }
 }
 
+extension ViewController: NSTableViewDelegate,NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        tasksList.count
+    }
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = taskTable.makeView(withIdentifier: .cell, owner: self) as! TasksTableViewCell
+        cell.taskTitel.stringValue = tasksList[row]
+        
+        return cell
+    }
+    func tableViewSelectionIsChanging(_ notification: Notification) {
+
+        selectedValue = taskTable.selectedRow
+    }
+    
+}
+
+extension NSUserInterfaceItemIdentifier {
+   static let cell = NSUserInterfaceItemIdentifier("Cell")
+}
